de03c42b86c79a8eed06a6c516bfa6d7006a4b51
Dawn Hopkins <dawnhopkins2479@gmail.com>
2021-12-13 21:35:44 -0500

    Repos_PocosAdded

---
+++ Komodo_App/Komodo_Repos/DevTeamRepo.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Komodo_Repos
+{
+    class DevTeamRepo
+    {
+        private Dictionary<int, DevTeam> _devTeamList;
+
+
+        public bool AddDevTeam(DevTeam devTeam)
+        {
+            bool bReturn = false;
+
+            if (!_devTeamList.ContainsKey(devTeam.TeamID))
+            {
+                _devTeamList.Add(devTeam.TeamID, devTeam);
+                bReturn = true;
+            }
+
+
+            return bReturn;
+
+        }
+        public bool RemoveDevTeam(DevTeam devTeam)
+        {
+            return (_devTeamList.Remove(devTeam.TeamID));
+        }
+        public bool UpdateDeveloper(DevTeam devTeam)
+        {
+            bool bReturn = false;
+
+            if (RemoveDevTeam(devTeam))
+            {
+                bReturn = AddDevTeam(devTeam);
+
+            }
+
+
+            return bReturn;
+        }
+    }
+}
---
+++ Komodo_App/Komodo_Repos/DevTeams.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Komodo_Repos
+{
+    class DevTeam
+    {
+        public string TeamName { get; set; }
+        public int TeamID { get; set; }
+
+        public DevTeam() { }
+
+        public DevTeam(string teamName, int teamId,)
+        {
+            TeamName = teamName;
+            TeamID = teamId;
+        }
+    }
+}
